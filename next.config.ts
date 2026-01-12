918d46f86abc3d2a6ac2ed1008b36791233e9895
Ashish <ashish@Yusufs-iMac.local>
2026-01-12 18:03:21 +0530

    disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
-    localPatterns: [
-      {
-        pathname: '/uploads/**',
-      },
-    ],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'staging-backend.islamic-relief.ch',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "staging-backend.islamic-relief.ch",
+        pathname: "/uploads/**",
       },
     ],
     dangerouslyAllowSVG: true,
-    unoptimized: process.env.NODE_ENV === 'development',
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  }
 };
 
 export default nextConfig;
